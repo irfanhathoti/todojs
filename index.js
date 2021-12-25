a629ffb3546c5864ccd02a4197ebe91a7305b7d3
irfanhathoti <irfanhathoti@gmail.com>
2021-12-25 16:23:59 +0530

    change

--- index.js
+++ index.js
@@ -1,184 +1,225 @@
-// OPEN ADD-LIST BOX
-document.querySelector("#list_Add_Btn_1").addEventListener("click", function () {
-    document.querySelector("#backGround").style.filter = "blur(15px)";
-    document.querySelector(".container").style.pointerEvents = "none";
-    document.querySelector("#dispay_List_Modal").classList.add("active");
-});
+let todoItems = [];
 
-// OPEN ADD-LIST BOX
-document.querySelector("#list_Add_Btn_2").addEventListener("click", function () {
-    document.querySelector("#backGround").style.filter = "blur(15px)";
-    document.querySelector(".container").style.pointerEvents = "none";
-    document.querySelector("#dispay_List_Modal").classList.add("active");
-});
+let currentChange;
+let flag = true;
 
-// CLOSE ADD-LIST BOX
-document.querySelector("#list_Back_Btn").addEventListener("click", function () {
-    document.querySelector("#backGround").style.filter = "none";
-    document.querySelector(".container").style.pointerEvents = "all";
-    document.querySelector("#dispay_List_Modal").classList.remove("active");
-});
+function initialisation() {
+  if (flag) {
+    document.getElementById("blur-2").style.display = "none";
+    document.getElementById("blur").style.display = "block";
+  } else {
+    document.getElementById("blur").style.display = "none";
+    document.getElementById("blur-2").style.display = "block";
+  }
+  if (todoItems.length === 0) {
+    console.log(document.getElementById("noTodo"));
+    console.log(todoItems);
+    document.getElementById("noitem").style.display = "block";
+  } else {
+    console.log("inside");
+    document.getElementById("noitem").style.display = "none";
+  }
+}
 
+initialisation();
 
+function renderTodo(todo) {
+  initialisation();
+  const list = document.querySelector(".flex-row-list");
+  var child = list.lastElementChild;
+  while (child) {
+    list.removeChild(child);
+    child = list.lastElementChild;
+  }
 
-// GETTING ALL REQUIRED ELEMENTS 
-
-const createListTitle = document.querySelector("#create_ListTitle");
-const createList = document.querySelector("#create_List");
-const listContainer = document.querySelector("#list_Container");
-let listBox = [];
-
-create_ListTitle.onkeypress = () => {
-    if (createListTitle.value.trim() != 0) {
-        createList.classList.add("active"); // active the add button
+  for (let i = 0; i < todoItems.length; i++) {
+    const node = document.createElement("div");
+    node.setAttribute("class", `card`);
+    node.setAttribute("data-key", todoItems[i].id);
+    node.innerHTML = `<p class="card-heading" onclick="redirect(this)">${todoItems[i].heading}</p>
+      <ul style="list-style-type:none;">
+      </ul>
+      <div class='footer'>
+          <button class='btn-completed' onclick="removeToDo(this)"><i class="fa fa-trash" aria-hidden="true"></i></button> 
+          <p class = 'btn-add' onclick="toggleAddItem(this)"><i class="fa fa-plus-circle"></i></p>
+      </div>
+      `;
+    console.log(node.childNodes);
+    list.append(node);
+    let currentTodo = todoItems[i];
+    for (let j = 0; j < currentTodo.subTask.length; j++) {
+      let classToPut = currentTodo.subTask[j].marked
+        ? "card-item card-item-checked"
+        : "card-item";
+      let rest = currentTodo.subTask[j].marked
+        ? ""
+        : '<button class = "markDone" onclick="markCompleted(this)">Mark Done</button>';
+      const liNode = document.createElement("li");
+      liNode.setAttribute("class", classToPut);
+      liNode.setAttribute("data-key", currentTodo.subTask[j].id);
+      liNode.innerHTML = ` ${currentTodo.subTask[j].name} ${rest}`;
+      node.childNodes[2].append(liNode);
     }
+  }
 }
 
+function markCompleted(element) {
+  let classToPut = flag
+    ? "card-item card-item-checked"
+    : "card-item-2 card-item-checked";
+  element.parentNode.setAttribute("class", classToPut);
+  let id = element.parentNode.parentNode.parentNode.getAttribute("data-key");
+  let subTaskId = element.parentNode.getAttribute("data-key");
 
-createList.onclick = () => {
-    let userData = createListTitle.value; // getting users value
-
-    const list = {
-        id: Date.now(),    // creating unique-id
-        name: userData,    // setting list-name
-        subTask: []
-    }
-
-    listBox.push(list);
-    
-    addList(); // invoking showList()
-    createNewTask();
-
-    createList.classList.remove("active"); // unactive the add button
-    createListTitle.value = ""; // once task added leave the input-field blank
-
-    document.querySelector("#backGround").style.filter = "none";
-    document.querySelector(".container").style.pointerEvents = "all";
-    document.querySelector("#dispay_List_Modal").classList.remove("active");
-
-    const noitm = document.getElementById("noitem");
-    noitm.style.display = "none"
-
-}
-
-
-// FUNCTION TO ADD A LIST INSIDE LIST-CONTAINER
-function addList() {
-    let newListTag = '';
-    listBox.forEach(element => {
-        newListTag += `
-        <div class="list-box" id="${element.id}">
-            <p class="list-heading">
-                <span>${element.name}</span>
-            </p>
-            <hr>
-            <ul class="items-container" id="${'id' + element.id}"></ul>
-            <span class="list-tools">
-                <span onclick="addTask(${element.id})">
-                <i class="fas fa-plus-circle"></i>
-                </span>
-                <span onclick="deleteList(${element.id})">
-                    <i class="fas fa-trash-alt"></i>
-                </span>
-            </span>
-        </div>`;
-    });
-    listContainer.innerHTML = newListTag; //adding new list's inside container
-
-    //CLICK THE CARD THAT CARD WILL BE SHOWN
-
-
-}
-
-addList(); // invoking showList()
-
-// FUNCTION TO DELETE A LIST
-function deleteList(id) {
-    listBox.forEach((element, index) => {
-        if (element.id === id) {
-            listBox.splice(index, 1);
+  // Find in the todo array
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id == id) {
+      for (let j = 0; j < todoItems[i].subTask.length; j++) {
+        if (todoItems[i].subTask[j].id == subTaskId) {
+          todoItems[i].subTask[j].marked = true;
         }
-    });
-    addList();
-    createNewTask();
-}
-
-// FUNCTION TO ADD A TASK
-function addTask(id) {
-
-    listBox.forEach(element => {
-        if(element.id === id) {
-            document.querySelector("#backGround").style.filter = "blur(15px)";
-            document.querySelector(".container").style.pointerEvents = "none";
-            document.querySelector("#display_Task_Modal").classList.add("active");
-
-            const taskTitle = document.querySelector("#create_TaskTitle");
-            const createTask = document.querySelector("#create_Task");
-            
-
-            taskTitle.onkeypress = () => {
-                if(taskTitle.value.trim() != 0) {
-                    createTask.classList.add("active");
-                }
-            }
-
-            createTask.onclick = () => {
-                let userTask = taskTitle.value;
-                
-                listBox.forEach((element, index) => {
-                    if(element.id === id) {
-                        const task ={
-                            taskID : Date.now(),
-                            taskName : userTask
-                        }
-                        listBox[index].subTask.push(task);
-                    }
-                });
-                createNewTask();
-                
-                createTask.classList.remove("active");
-                taskTitle.value = ""; 
-                
-                document.querySelector("#backGround").style.filter = "none";
-                document.querySelector(".container").style.pointerEvents = "all";
-                document.querySelector("#display_Task_Modal").classList.remove("active");
-            }
-        }
-    });
-    // CLOSE ADD-TASK POP-UP
-    document.querySelector("#task_Back_Btn").onclick = () => {
-        document.querySelector("#backGround").style.filter = "none";
-        document.querySelector(".container").style.pointerEvents = "all";
-        document.querySelector("#display_Task_Modal").classList.remove("active");
-    
+      }
     }
+  }
+  element.parentNode.removeChild(element);
 }
 
-function createNewTask() {
-    listBox.forEach(element => {
-        let taskContainer = document.getElementById('id' + element.id);
-        let newTaskTag = '';
-        element.subTask.forEach(task => {
-            newTaskTag += `
-            <li>
-                <span class="task-name" id="${'tid' + task.taskID}">${task.taskName}</span>
-                <i class="fas fa-check" id="${'iid' + task.taskID}" onclick=strikeOff(${task.taskID})></i>
-            </li>`;
-        })
-        taskContainer.innerHTML = newTaskTag;
-    })
+function addTodo() {
+  let heading = document.getElementById("listHeading").value;
+  if (heading !== "") {
+    const todo = {
+      heading,
+      completed: false,
+      subTask: [],
+      id: Date.now(),
+    };
+    todoItems.push(todo);
+    toggle();
+    goBack();
+  }
 }
 
-// // FUNCTION TO DELETE A LIST
-function strikeOff(id) {
-    listBox.forEach(element => {
-        element.subTask.forEach(task => {
-            if(task.taskID === id) {
-                document.getElementById('tid' + task.taskID).style.textDecoration = "line-through";
-                document.getElementById('iid' + task.taskID).style.visibility = "hidden";
-            }
-        })
+function addSubTodo() {
+  let taskHeading = document.getElementById("subListHeading").value;
+  if (taskHeading !== "") {
+    let list;
+    if (flag) {
+      list = currentChange.parentNode.parentNode.childNodes[2];
+    } else {
+      list = currentChange.parentNode.parentNode.childNodes[3];
+    }
+    console.log(currentChange.parentNode, currentChange.parentNode.parentNode);
+    let id = currentChange.parentNode.parentNode.getAttribute("data-key");
+    console.log(currentChange.parentNode.parentNode);
 
-    });
+    const node = document.createElement("li");
+    node.setAttribute("class", flag ? `card-item` : `card-item-2`);
+    node.setAttribute("data-key", Date.now());
+    node.innerHTML = ` ${taskHeading}<button class = 'markDone' onclick="markCompleted(this)">Mark Done</button>`;
+
+    let currentTodo;
+    //Find in the todo array
+    for (let i = 0; i < todoItems.length; i++) {
+      if (todoItems[i].id == id) {
+        todoItems[i].subTask.push({
+          name: taskHeading,
+          marked: false,
+          id: node.getAttribute("data-key"),
+        });
+      }
+    }
+
+    list.append(node);
+    toggleAddItem();
+  }
+  console.log(todoItems);
 }
 
+function removeToDo(element) {
+  let tempElement = element.parentNode.parentNode;
+  console.log(tempElement);
+
+  //Find in the todo array and remove
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id == tempElement.getAttribute("data-key")) {
+      todoItems.splice(i, 1);
+    }
+  }
+  if (!flag) {
+    goBack();
+  } else {
+    tempElement.parentNode.removeChild(tempElement);
+    initialisation();
+  }
+}
+
+function toggle() {
+  var blur;
+  if (flag) {
+    blur = document.getElementById("blur");
+  } else {
+    blur = document.getElementById("blur-2");
+  }
+  blur.classList.toggle("active");
+
+  var popup = document.getElementById("pop");
+  popup.classList.toggle("active");
+}
+
+function toggleAddItem(item) {
+  currentChange = item;
+  var blur;
+  if (flag) {
+    blur = document.getElementById("blur");
+  } else {
+    blur = document.getElementById("blur-2");
+  }
+  blur.classList.toggle("active");
+
+  var popup = document.getElementById("popAddItem");
+  popup.classList.toggle("active");
+}
+
+function redirect(element) {
+  let id = element.parentNode.getAttribute("data-key");
+
+  let currentTodo;
+  //Find in the todo array
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id == id) {
+      currentTodo = todoItems[i];
+    }
+  }
+  flag = false;
+  initialisation();
+  document.getElementById("currentHeading").textContent = currentTodo.heading;
+  document.getElementById("currentHeading-1").textContent = currentTodo.heading;
+  document
+    .getElementById("currentHeading-1")
+    .parentNode.setAttribute("data-key", currentTodo.id);
+
+  console.log(currentTodo);
+  let e = document.getElementById("singleList");
+  var child = e.lastElementChild;
+  while (child) {
+    e.removeChild(child);
+    child = e.lastElementChild;
+  }
+  for (let i = 0; i < currentTodo.subTask.length; i++) {
+    let classToPut = currentTodo.subTask[i].marked
+      ? "card-item-2 card-item-checked"
+      : "card-item-2";
+    let rest = currentTodo.subTask[i].marked
+      ? ""
+      : '<button class = "markDone" onclick="markCompleted(this)">Mark Done</button>';
+    const node = document.createElement("li");
+    node.setAttribute("class", classToPut);
+    node.setAttribute("data-key", currentTodo.subTask[i].id);
+    node.innerHTML = ` ${currentTodo.subTask[i].name} ${rest}`;
+    e.append(node);
+  }
+}
+function goBack() {
+  flag = true;
+  renderTodo();
+}
